c0f8beff4226ce7a313e0b4dd7315ab9fcaed154
Sedonia <river.sedonia@gmail.com>
2024-12-13 13:59:12 -0500

    update app.js, index.html, and style.css. add image files

--- src/app.js
+++ src/app.js
@@ -1,11 +1,132 @@
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
+import "../node_modules/bootstrap";
+// import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+// import clubsImageTag from "./assets/img/club.svg";
+// import diamondsImageTag from "./assets/img/diamond.svg";
+// import heartsImageTag from "./assets/img/heart.svg";
+// import spadesImageTag from "./assets/img/spade.svg";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const newDeckButton = document.querySelector("#newDeck");
+  const newCardButton = document.querySelector("#nextCard");
+  newDeckButton.addEventListener("click", refreshPage);
+  newDeckButton.style.display = "block";
+  newCardButton.addEventListener("click", addNewCardToDeck);
+  newCardButton.style.display = "block";
+
+  const cardBackDiv = document.querySelector("#card-back");
+  const cardBack = document.createElement("img");
+  cardBack.setAttribute("src", "../src/assets/img/card_back2.jpg");
+  cardBack.setAttribute("class", "card-back-image");
+  cardBackDiv.append(cardBack);
+
+  const heartsImageTag = "../src/assets/img/heart.svg";
+  const spadesImageTag = "../src/assets/img/spade.svg";
+  const clubsImageTag = "../src/assets/img/club.svg";
+  const diamondsImageTag = "../src/assets/img/diamond.svg";
+  const suitTags = [
+    heartsImageTag,
+    spadesImageTag,
+    clubsImageTag,
+    diamondsImageTag
+  ];
+  const cardValues = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A"
+  ];
+
+  function chooseRandomIndex(array) {
+    const randomIndex = Math.floor(Math.random() * array.length);
+    return randomIndex;
+  }
+
+  function refreshPage() {
+    window.location.reload();
+  }
+
+  function buildTopOrBottomRow(sideOfCard, value, suit) {
+    const topOrBottomRow = document.createElement("div");
+
+    if (sideOfCard === "top") {
+      topOrBottomRow.setAttribute("class", "top-and-bottom");
+    } else if (sideOfCard === "bottom") {
+      topOrBottomRow.setAttribute("class", "top-and-bottom rotated");
+    }
+
+    const p = document.createElement("p");
+    const strong = document.createElement("strong");
+    const image = document.createElement("img");
+    image.setAttribute("class", "corner-img");
+    const cardValue = document.createTextNode(value);
+    strong.appendChild(cardValue);
+    p.appendChild(strong);
+    image.src = suit;
+    topOrBottomRow.appendChild(p);
+    topOrBottomRow.appendChild(image);
+    return topOrBottomRow;
+  }
+
+  function buildMiddleRow(suit) {
+    const middleRow = document.createElement("div");
+    middleRow.setAttribute("class", "middle");
+    const image = document.createElement("img");
+    image.setAttribute("class", "center-img");
+    image.src = suit;
+    middleRow.appendChild(image);
+    return middleRow;
+  }
+
+  const allCardOptions = [];
+  for (let suitTag in suitTags) {
+    for (let cardValue in cardValues) {
+      let tagPlusValue = [];
+      tagPlusValue.push(`${suitTags[suitTag]}`);
+      tagPlusValue.push(`${cardValues[cardValue]}`);
+      allCardOptions.push(tagPlusValue);
+    }
+  }
+
+  function selectSuitAndValueFromAllOptions(array) {
+    const randomIndex = chooseRandomIndex(array);
+    return array.splice(randomIndex, 1);
+  }
+
+  function constructEachRandomCard(array) {
+    const suitAndValue = selectSuitAndValueFromAllOptions(array);
+    const suit = suitAndValue[0][0];
+    const value = suitAndValue[0][1];
+    const card = document.createElement("div");
+    card.setAttribute("class", "card");
+    const newTopRow = buildTopOrBottomRow("top", value, suit);
+    const newMiddleRow = buildMiddleRow(suit);
+    const newBottomRow = buildTopOrBottomRow("bottom", value, suit);
+    card.appendChild(newTopRow);
+    card.appendChild(newMiddleRow);
+    card.appendChild(newBottomRow);
+    return card;
+  }
+
+  function addNewCardToDeck() {
+    if (allCardOptions.length !== 0) {
+      const newCard = constructEachRandomCard(allCardOptions);
+      const boxDiv = document.querySelector(".box");
+      boxDiv.appendChild(newCard);
+    } else if (allCardOptions.length === 0) {
+      alert("That's all 52 cards! Press 'New Deck' to start again.");
+    }
+  }
+
+  console.log("Welcome to Sedonia's Random Card Generator (52 card deck)");
 };
